a91236c43dd89c788818fa4fa3a8b2ba64121af2
Hiroki Terashima <honchikun@gmail.com>
2012-03-02 16:36:43 -0800

    moved settings to defs.php

--- vle/post.php
+++ vle/post.php
@@ -1,14 +1,16 @@
 <?php
-$dir = "/Users/hirokiterashima/Sites/translate/WISE-Translate/vle/view/i18n/";
+include_once("../defs.php");
 if (isset($_POST["locale"]) && isset($_POST["jsonStr"])) {
-  $filePath = $dir."i18n_".$_POST["locale"].".json";
-  $result = file_put_contents($filePath,stripslashes($_POST["jsonStr"]));
+  $filePath = $i18n_dir."i18n_".$_POST["locale"].".json";
+  $jsonString = stripslashes($_POST["jsonStr"]);
+  $result = file_put_contents($filePath,$jsonString);
   if ($result === FALSE) {
     echo "FAIL";
     exit;
   } else {
-    // run exec to commit changes to github
-    exec("./commit.sh");
+    // run exec to commit changes to github...ran into permission problems. can't do this, apparently.
+    // so send email each time
+    mail($adminEmail,"VLE translation locale: ".$_POST["locale"], $jsonString);
     exit;
   }
 }
