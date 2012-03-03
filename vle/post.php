1e3401e38dd4a0b3f52812e5aca466926aea9d1e
Hiroki Terashima <honchikun@gmail.com>
2012-03-02 16:17:35 -0800

    translations

--- vle/post.php
+++ vle/post.php
@@ -2,11 +2,14 @@
 $dir = "/Users/hirokiterashima/Sites/translate/WISE-Translate/vle/view/i18n/";
 if (isset($_POST["locale"]) && isset($_POST["jsonStr"])) {
   $filePath = $dir."i18n_".$_POST["locale"].".json";
-  echo file_put_contents($filePath,stripslashes($_POST["jsonStr"]));
-  exit;
-
-  // todo: call git functions via exec()
+  $result = file_put_contents($filePath,stripslashes($_POST["jsonStr"]));
+  if ($result === FALSE) {
+    echo "FAIL";
+    exit;
+  } else {
+    // run exec to commit changes to github
+    exec("./commit.sh");
+    exit;
+  }
 }
-
-echo 'yo';
 ?>
